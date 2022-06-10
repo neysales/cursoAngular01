2c788c6ad1549e586521bebcdbda84aea7b81ee6
WDEV\juceny.sales <juceny.sales@ebix.com>
2022-06-10 19:56:39 -0300

    lista

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero.module";
+
+export const HEROES: Hero[] = [
+  {id: 1, name:'Wolverine'},
+  {id: 2, name:'Super Homem'},
+  {id: 3, name:'Homem Aranha'},
+  {id: 4, name:'Thor'},
+  {id: 5, name:'Hulk'},
+  {id: 6, name:'Batman'},
+  {id: 7, name:'Mulher Gato'},
+  {id: 8, name:'Capitão América'},
+  {id: 9, name:'Mulher Maravilha'},
+];
